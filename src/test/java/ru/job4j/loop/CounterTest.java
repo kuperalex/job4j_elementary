8cd5a97c7bf489e13425fcef4afe8e2651193fac
Alex <kuperalex@gmail.com>
2025-06-07 00:24:40 +0300

    Подсчет суммы чётных чисел в диапазоне [#505210]

--- src/test/java/ru/job4j/loop/CounterTest.java
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -7,42 +7,38 @@ import static org.assertj.core.api.Assertions.*;
 class CounterTest {
 
     @Test
-    void whenStartLessFinishThenResultFiftyFive() {
-        int start = 0, finish = 10;
-        int expected = 55;
-        int output = Counter.sum(start, finish);
-        assertThat(output).isEqualTo(expected);
+    void whenSumEvenNumbersFromOneToTenThenThirty() {
+        int start = 1;
+        int finis = 10;
+        int result = Counter.sumByEven(start, finis);
+        int expected = 30;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenStartLessFinishThenResultThirtyThree() {
-        int start = 3, finish = 8;
-        int expected = 33;
-        int output = Counter.sum(start, finish);
-        assertThat(output).isEqualTo(expected);
+    void whenSumEvenNumbersFromFiveToTwelveThenThirtySix() {
+        int start = 5;
+        int finis = 12;
+        int result = Counter.sumByEven(start, finis);
+        int expected = 36;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenStartLessFinishThenResultFifteen() {
-        int start = 0, finish = 5;
-        int expected = 15;
-        int output = Counter.sum(start, finish);
-        assertThat(output).isEqualTo(expected);
+    void whenSumEvenNumbersFromTenToTwentyThenThirtyNinety() {
+        int start = 10;
+        int finis = 20;
+        int result = Counter.sumByEven(start, finis);
+        int expected = 90;
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenStartLessFinishThenResultOne() {
-        int start = 1, finish = 1;
-        int expected = 1;
-        int output = Counter.sum(start, finish);
-        assertThat(output).isEqualTo(expected);
-    }
-
-    @Test
-    void whenStartMoreFinishThenReultZero() {
-        int start = 5, finish = 3;
-        int expected = 0;
-        int output = Counter.sum(start, finish);
-        assertThat(output).isEqualTo(expected);
+    void whenSumEvenNumbersFromMinusFiveToEightThenFourTeen() {
+        int start = -5;
+        int finis = 8;
+        int result = Counter.sumByEven(start, finis);
+        int expected = 14;
+        assertThat(result).isEqualTo(expected);
     }
 }
